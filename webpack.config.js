21613073100252cb3e7a8b75220b0f867f5ead41
hexx.one <5312542+hexxone@users.noreply.github.com>
2020-11-24 20:56:58 +0100

    WebWorker fixxes

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,22 @@
 const path = require('path');
 
-const CircularDependencyPlugin = require('circular-dependency-plugin')
+//const CircularDependencyPlugin = require('circular-dependency-plugin')
 
 module.exports = {
   entry: {
-    magicIO: './dist/babel/AudiOrbits.js'
+    audiorbits: './dist/babel/AudiOrbits.js'
   },
   output: {
-    filename: 'AudiOrbits.bundle.js',
+    chunkFilename: '[id].bundle.js',
     path: path.resolve(__dirname, 'dist') + '/pack/js'
   },
-  devtool: "source-map",
+  //devtool: "source-map",
   devServer: {
     contentBase: path.join(__dirname, 'dist') + '/pack',
     port: 9000
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
   },
   module: {
     rules: [
@@ -23,26 +26,12 @@ module.exports = {
           {
             loader: 'worker-loader',
             options: {
-              esModule: false,
+              esModule: true,
+              chunkFilename: '[id].[chunkhash].worker.js',
             },
           },
         ],
       }
     ]
-  },
-  plugins: [
-    new CircularDependencyPlugin({
-      // exclude detection of files based on a RegExp
-      exclude: /a\.js|node_modules/,
-      // include specific files based on a RegExp
-      include: /dir/,
-      // add errors to webpack instead of warnings
-      failOnError: true,
-      // allow import cycles that include an asyncronous import,
-      // e.g. via import(/* webpackMode: "weak" */ './file.js')
-      allowAsyncCycles: false,
-      // set the current working directory for displaying module paths
-      cwd: process.cwd(),
-    })
-  ]
+  }
 };
