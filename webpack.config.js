1a26727352162d703deded2bf380359e7a63adac
hexx.one <5312542+hexxone@users.noreply.github.com>
2021-10-23 10:18:44 +0200

    working preparation for three.ts

--- webpack.config.js
+++ webpack.config.js
@@ -4,18 +4,39 @@
 * Probably not a good example to start from :D
 */
 
+// system
 const path = require('path');
 
+// custom
 const OfflinePlugin = require('./src/we_utils/src/offline/OfflinePlugin');
 const WascBuilderPlugin = require('./src/we_utils/src/wasc-worker/WascBuilderPlugin');
 const RenamerPlugin = require('./src/we_utils/src/renamer/RenamerPlugin');
 
+// optimizing
 const propertiesRenameTransformer = require('ts-transformer-properties-rename').default;
-
-const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
-
 const TerserPlugin = require('terser-webpack-plugin');
+const ThreeMinifierPlugin = require('@yushijinhun/three-minifier-webpack');
+const threeMinifier = new ThreeMinifierPlugin();
 
+// analyzing
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+// prop list
+const propss = Object.keys(JSON.parse(require('fs').readFileSync('./public/project.json')).general.properties);
+propss.push('general', 'supportsaudioprocessing', 'preview', 'visibility', 'workshopid',
+	'condition', 'editable', 'min', 'max', 'order', 'text', 'type', 'value', 'title', 'tags',
+	'localization', 'description', 'approved', 'file', 'path', 'language', 'properties', 'time',
+	'module', 'instance', 'exports', 'params', 'run', 'env', 'memory', 'payload', 'id',
+	'action', 'func', 'data', 'set', 'get', 'buffer', 'props', 'result', 'ellapsed',
+	'levelSettings', 'audioSettings', 'build', 'update', 'outputData', 'audioProps', 'audioprocessing',
+	'crossOriginIsolated', 'initial', 'maximum', 'shared', 'common', 'vertex', 'vertexShader', 'fragment', 'fragmentShader',
+	'normal', 'position', 'uv', 'precision',
+	'ascender', 'boundingBox', 'yMin', 'xMin', 'yMax', 'xMax', 'cssFontStyle', 'cssFontWeight', 'descender', 'familyName',
+	'glyphs', 'original_font_information', 'format', 'fontFamily', 'fontSubfamily', 'uniqueID', 'fullName', 'resolution',
+	'underlinePosition', 'underlineThickness', '__getFloat64ArrayView', '__getFloat32ArrayView', '__getInt32Array');
+
+// const
 const ENTRY_FILE = './src/AudiOrbi.ts';
 const BUILD_PATH = path.resolve(__dirname, 'dist');
 
@@ -23,10 +44,10 @@ module.exports = (env) => {
 	const prod = env.production || false;
 
 	return {
-		mode: 'production',
+		mode: prod ? 'production' : 'development',
 		entry: {
-			// audiorbits: './dist/tsc/AudiOrbits.js',
-			audiorbits: ENTRY_FILE,
+			// ao: './dist/tsc/AudiOrbits.js',
+			ao: ENTRY_FILE,
 		},
 		output: {
 			// libraryTarget: 'commonjs2',
@@ -39,16 +60,22 @@ module.exports = (env) => {
 			headers: {
 				'Access-Control-Allow-Origin': '*',
 				'https': true,
+				'Cross-Origin-Opener-Policy': 'same-origin',
+				'Cross-Origin-Embedder-Policy': 'require-corp',
 			},
 		},
 		resolve: {
 			extensions: ['.ts', '.js'],
+			plugins: [
+				// THREE.JS tree shaking helper
+				threeMinifier.resolver,
+			],
 		},
 		module: {
 			rules: [
 				// TypeScript loader
 				{
-					test: /\.tsx?$/,
+					test: /\.(t)sx?$/,
 					loader: require.resolve('awesome-typescript-loader'), // || ts-loader
 					options: {
 						compiler: 'ttypescript',
@@ -56,13 +83,17 @@ module.exports = (env) => {
 						getCustomTransformers: (program) => {
 							return {
 								before: [
-									propertiesRenameTransformer(program, {entrySourceFiles: [ENTRY_FILE]}),
+									propertiesRenameTransformer(program, {
+										entrySourceFiles: [
+											ENTRY_FILE,
+										],
+									}),
 								],
 							};
 						},
 					},
 				},
-				// use a specific loader for workers...
+				// loader for workers...
 				{
 					test: /\.worker\.(c|m)?js$/i,
 					loader: 'worker-loader',
@@ -70,6 +101,11 @@ module.exports = (env) => {
 						esModule: true,
 						chunkFilename: '[id].[chunkhash].worker.js',
 					},
+				},
+				// ts shader loader
+				{
+					test: /\.(glsl)$/,
+					loader: require.resolve('./src/we_utils/src/three/shader/loader'),
 				},
 				// exclude .asc from the bundle
 				{
@@ -87,6 +123,8 @@ module.exports = (env) => {
 		target: ['es2020'],
 		// plugins
 		plugins: [
+			// THREE.JS tree shaking helper
+			threeMinifier,
 			// The TS Syntax checking
 			new ForkTsCheckerWebpackPlugin({
 
@@ -110,6 +148,12 @@ module.exports = (env) => {
 				extrafiles: ['/'],
 				pretty: !prod,
 			}),
+			// webpack bundle analyzer
+			new BundleAnalyzerPlugin({
+				analyzerMode: 'static',
+				reportFilename: '../out/ao_bundle_report.html',
+				openAnalyzer: false,
+			}),
 			// custom renamer
 			new RenamerPlugin({
 				regex: /[a-z0-9_]*_webpack_[a-z0-9_]*/gi,
@@ -117,10 +161,8 @@ module.exports = (env) => {
 		],
 		// remove dead code in production
 		optimization: prod ? {
-			minimize: true,
-			mangleExports: true,
-			usedExports: true,
 			nodeEnv: 'production',
+			minimize: true,
 			minimizer: [
 				new TerserPlugin({
 					// https://github.com/webpack-contrib/terser-webpack-plugin#terseroptions
@@ -130,18 +172,30 @@ module.exports = (env) => {
 						compress: {
 							unsafe: true,
 							pure_funcs: ['console.warn'], // ~40kb of three.js messages... errors will stil come through.
+							hoist_funs: true,
 						},
 						mangle: {
 							properties: {
-								regex: /_(private|internal|webpack|WEBPACK)_/, // the same prefixes like for custom transformer
+								keep_quoted: true,
+								reserved: propss,
+								// regex: /_(private|internal)_/, // the same prefixes like for custom transformer
 							},
 						},
 						module: false,
 						toplevel: true,
 						sourceMap: false,
+						keep_fnames: false,
 					},
 				}),
 			],
+			moduleIds: 'size',
+			mangleExports: 'size',
+			mangleWasmImports: true,
+			providedExports: true,
+			usedExports: true,
+			concatenateModules: true,
+			sideEffects: true,
+			innerGraph: true,
 		}: {},
 	};
 };
