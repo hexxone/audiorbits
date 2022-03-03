15f8cc5218768a1c77000d9dc92595fbcc6361c7
hexx.one <dominics.pc@gmail.com>
2022-03-03 12:52:58 +0100

    add "prettier" config & formatting

--- src/AudiOrbi.ts
+++ src/AudiOrbi.ts
@@ -1,68 +1,100 @@
 /**
-* @author hexxone / https://hexx.one
-*
-* @license
-* Copyright (c) 2021 hexxone All rights reserved.
-* Licensed under the GNU GENERAL PUBLIC LICENSE.
-* See LICENSE file in the project root for full license information.
-*
-* @see
-* AudiOrbits project	/ https://steamcommunity.com/sharedfiles/filedetails/?id=1396475780
-* for Wallpaper Engine / https://steamcommunity.com/app/431960
-* Code Repository:     / https://github.com/hexxone/audiorbits
-* Online Preview:		/ https://orbits.hexx.one
-*
-* @description
-* Audiorbits Web-Wallpaper for Wallpaper Engine
-*
-* If you're reading this you're either pretty interested in the code or just bored :P
-* Either way thanks for using this Wallpaper!
-* Feel free to leave me some feedback on the Workshop-Page if you like :)
-*/
+ * @author hexxone / https://hexx.one
+ *
+ * @license
+ * Copyright (c) 2021 hexxone All rights reserved.
+ * Licensed under the GNU GENERAL PUBLIC LICENSE.
+ * See LICENSE file in the project root for full license information.
+ *
+ * @see
+ * AudiOrbits project	/ https://steamcommunity.com/sharedfiles/filedetails/?id=1396475780
+ * for Wallpaper Engine / https://steamcommunity.com/app/431960
+ * Code Repository:     / https://github.com/hexxone/audiorbits
+ * Online Preview:		/ https://orbits.hexx.one
+ *
+ * @description
+ * Audiorbits Web-Wallpaper for Wallpaper Engine
+ *
+ * If you're reading this you're either pretty interested in the code or just bored :P
+ * Either way thanks for using this Wallpaper!
+ * Feel free to leave me some feedback on the Workshop-Page if you like :)
+ */
 /* eslint-disable no-unused-vars */
 
-import {ContextHelper} from './ContextHelper';
-import {WEventListener} from './WEventListener';
+import { ContextHelper } from "./ContextHelper";
+import { WEventListener } from "./WEventListener";
 
-import {CComponent, CSettings, ReloadHelper, rgbToObj, Smallog, waitReady, WarnHelper, WEWWA, LoadHelper} from './we_utils/src';
+import {
+	CComponent,
+	CSettings,
+	ReloadHelper,
+	rgbToObj,
+	Smallog,
+	waitReady,
+	WarnHelper,
+	WEWWA,
+	LoadHelper,
+} from "./we_utils/src";
 
-const Ignore: string[] = ['img_overlay', 'img_background', 'mirror_invalid_val',
-	'wec_brs', 'wec_con', 'wec_e', 'wec_hue', 'wec_sa', '_d0'];
+const Ignore: string[] = [
+	"img_overlay",
+	"img_background",
+	"mirror_invalid_val",
+	"wec_brs",
+	"wec_con",
+	"wec_e",
+	"wec_hue",
+	"wec_sa",
+	"_d0",
+];
 
-const ReInit: string[] = ['geometry_type', 'base_texture', 'scaling_factor', 'num_levels',
-	'level_depth', 'level_shifting', 'level_spiralize', 'num_subsets_per_level', 'num_points_per_subset',
-	'custom_fps', 'field_of_view', 'icue_mode', 'shader_quality', 'random_seed', 'low_latency', 'xr_mode'];
-
+const ReInit: string[] = [
+	"geometry_type",
+	"base_texture",
+	"scaling_factor",
+	"num_levels",
+	"level_depth",
+	"level_shifting",
+	"level_spiralize",
+	"num_subsets_per_level",
+	"num_points_per_subset",
+	"custom_fps",
+	"field_of_view",
+	"icue_mode",
+	"shader_quality",
+	"random_seed",
+	"low_latency",
+	"xr_mode",
+];
 
 // temporary properties
 let temProps = null;
-window['wallpaperPropertyListener'] = {
+window["wallpaperPropertyListener"] = {
 	applyUserProperties: (p) => {
-		console.log('Before', p);
+		console.log("Before", p);
 		temProps = p;
 	},
 };
 
-
 /**
-* what's the wallpaper currently doing?
-*/
+ * what's the wallpaper currently doing?
+ */
 enum RunState {
 	None = 0,
 	Initializing = 1,
 	Running = 2,
 	Paused = 3,
-	ReInitializing = 4
+	ReInitializing = 4,
 }
 
 /**
-* Root Settings
-* @public
-*/
+ * Root Settings
+ * @public
+ */
 class MainSettings extends CSettings {
 	debugging = false;
 	// default scheme property
-	schemecolor = '0 0 0';
+	schemecolor = "0 0 0";
 	// Misc category
 	seizure_warning = true;
 	// mirrored setting
@@ -71,8 +103,8 @@ class MainSettings extends CSettings {
 }
 
 /**
-* Root Class
-*/
+ * Root Class
+ */
 class AudiOrbits extends CComponent {
 	// holds default wallpaper settings
 	// these basically connect 1:1 to wallpaper engine settings.
@@ -101,12 +133,12 @@ class AudiOrbits extends CComponent {
 	private weListener: WEventListener = null;
 
 	/**
-	* Intialize Wallpaper...
-	*/
+	 * Intialize Wallpaper...
+	 */
 	constructor() {
 		super();
-		Smallog.setPrefix('[AudiOrbits] ');
-		Smallog.debug('constructing...');
+		Smallog.setPrefix("[AudiOrbits] ");
+		Smallog.debug("constructing...");
 
 		this.children.push(this.ctxHolder);
 		this.children.push(this.warnHelper);
@@ -114,7 +146,7 @@ class AudiOrbits extends CComponent {
 
 		// will apply settings edited in Wallpaper Engine
 		// this will also cause initialization for the first time
-		window['wallpaperPropertyListener'] = this.weListener = {
+		window["wallpaperPropertyListener"] = this.weListener = {
 			applyUserProperties: (props) => {
 				const initFlag = this.applyCustomProps(props);
 				// very first initialization
@@ -123,9 +155,12 @@ class AudiOrbits extends CComponent {
 					waitReady().then(() => this.initOnce());
 				} else if (initFlag) {
 					this.state = RunState.ReInitializing;
-					Smallog.debug('got reInit-flag from applying settings!');
+					Smallog.debug("got reInit-flag from applying settings!");
 					if (this.resetTimeout) clearTimeout(this.resetTimeout);
-					this.resetTimeout = setTimeout(() => this.reInitSystem(), this.resetTimespan * 1000);
+					this.resetTimeout = setTimeout(
+						() => this.reInitSystem(),
+						this.resetTimespan * 1000
+					);
 					// show reloader
 					this.reloadHelper.show(true);
 					// stop frame animation
@@ -135,7 +170,8 @@ class AudiOrbits extends CComponent {
 
 			setPaused: (isPaused: boolean) => {
 				// only pause/running toggle, ignore other states
-				if (this.state != RunState.Running && this.state != RunState.Paused) return;
+				if (this.state != RunState.Running && this.state != RunState.Paused)
+					return;
 
 				if (this.state == RunState.Paused) {
 					if (isPaused) return;
@@ -144,14 +180,20 @@ class AudiOrbits extends CComponent {
 					if (!isPaused) return;
 					this.state = RunState.Paused;
 				}
-				Smallog.debug('set pause: ' + isPaused);
+				Smallog.debug("set pause: " + isPaused);
 				this.ctxHolder.setRenderer(!isPaused);
 			},
 
 			// currently not used
-			applyGeneralProperties: (props) => { return; },
-			userDirectoryFilesAddedOrChanged: (p, f) => { return; },
-			userDirectoryFilesRemoved: (p, f) => { return; },
+			applyGeneralProperties: () => {
+				return;
+			},
+			userDirectoryFilesAddedOrChanged: () => {
+				return;
+			},
+			userDirectoryFilesRemoved: () => {
+				return;
+			},
 		};
 
 		if (temProps) this.weListener.applyUserProperties(temProps);
@@ -162,12 +204,12 @@ class AudiOrbits extends CComponent {
 	// /////////////////////////////////////////////
 
 	/**
-	* Apply settings from the project.json "properties" object and takes certain actions
-	* @param {Object} props Properties
-	* @return {boolean} reinit-flag
-	*/
+	 * Apply settings from the project.json "properties" object and takes certain actions
+	 * @param {Object} props Properties
+	 * @return {boolean} reinit-flag
+	 */
 	private applyCustomProps(props) {
-		Smallog.debug('applying settings: ' + JSON.stringify(props));
+		Smallog.debug("applying settings: " + JSON.stringify(props));
 
 		// possible apply-targets
 		let reInitFlag = false;
@@ -175,7 +217,12 @@ class AudiOrbits extends CComponent {
 		// loop all settings for updated values
 		for (const setting in props) {
 			// ignore this setting or apply it manually
-			if (Ignore.indexOf(setting) > -1 || setting.indexOf('HDR_') === 0 || setting.indexOf('SPCR_') === 0) continue;
+			if (
+				Ignore.indexOf(setting) > -1 ||
+				setting.indexOf("HDR_") === 0 ||
+				setting.indexOf("SPCR_") === 0
+			)
+				continue;
 			// get the updated setting
 			const prop = props[setting];
 			// check typing
@@ -183,22 +230,26 @@ class AudiOrbits extends CComponent {
 
 			let found = false;
 			// apply prop value
-			switch (prop.type || 'none') {
-			case 'bool':
-				found = this.applySetting(setting, prop.value == true || prop.value == 'true' || prop.value == 'True');
-				break;
-			case 'slider':
-			case 'combo':
-				found = this.applySetting(setting, parseFloat(prop.value));
-				break;
-			default:
-				found = this.applySetting(setting, prop.value || prop.text);
-				break;
+			switch (prop.type || "none") {
+				case "bool":
+					found = this.applySetting(
+						setting,
+						prop.value == true || prop.value == "true" || prop.value == "True"
+					);
+					break;
+				case "slider":
+				case "combo":
+					found = this.applySetting(setting, parseFloat(prop.value));
+					break;
+				default:
+					found = this.applySetting(setting, prop.value || prop.text);
+					break;
 			}
 			// set re-init flag if value changed and included in list
 			if (found) reInitFlag ||= ReInit.indexOf(setting) > -1;
 			// invalid?
-			else if (prop.type != 'text') Smallog.debug('Setting not applied: ' + setting);
+			else if (prop.type != "text")
+				Smallog.debug("Setting not applied: " + setting);
 		}
 
 		// Update all modules
@@ -212,64 +263,65 @@ class AudiOrbits extends CComponent {
 
 		// Custom user images
 		if (props.img_background) {
-			this.setImgSrc('img_back', props.img_background.value);
+			this.setImgSrc("img_back", props.img_background.value);
 		}
 		if (props.img_overlay) {
-			this.setImgSrc('img_over', props.img_overlay.value);
+			this.setImgSrc("img_over", props.img_overlay.value);
 		}
 
 		// debug
 		// Smallog.setLevel(this.settings.debugging ? LogLevel.Debug : LogLevel.Info);
-		const dbgWnd = document.getElementById('debugwnd');
-		if (this.settings.debugging) dbgWnd.classList.add('show');
-		else dbgWnd.classList.remove('show');
-
+		const dbgWnd = document.getElementById("debugwnd");
+		if (this.settings.debugging) dbgWnd.classList.add("show");
+		else dbgWnd.classList.remove("show");
 
 		// have render-relevant settings been changed?
 		return reInitFlag;
 	}
 
 	/**
-	* Set Image
-	* @param {string} imgID html element id
-	* @param {string} srcVal new src value
-	*/
+	 * Set Image
+	 * @param {string} imgID html element id
+	 * @param {string} srcVal new src value
+	 * @returns {void}
+	 */
 	private setImgSrc(imgID: string, srcVal: string) {
 		const elmt = document.getElementById(imgID);
-		elmt.classList.remove('show');
+		elmt.classList.remove("show");
 		if (!srcVal) return;
 		setTimeout(() => {
 			// "file:///" +
-			elmt.setAttribute('src', 'file:///' + srcVal);
-			elmt.classList.add('show');
+			elmt.setAttribute("src", "file:///" + srcVal);
+			elmt.classList.add("show");
 		}, 1000);
 	}
 
 	/**
-	* Update Debugging State
-	* @public
-	* @return {Object} null
-	*/
+	 * Update Debugging State
+	 * @public
+	 * @return {Object} null
+	 */
 	public updateSettings(): Promise<void> {
 		return Promise.resolve();
 	}
-
 
 	// /////////////////////////////////////////////
 	// INITIALIZE
 	// /////////////////////////////////////////////
 
 	/**
-	* do first init after page loaded
-	*/
+	 * do first init after page loaded
+	 * @returns {void}
+	 */
 	private initOnce() {
 		// initializing and wait for seizure warning
 		this.initSystem(this.warnHelper.show());
 	}
 
 	/**
-	* re-initialies the walpaper after some time
-	*/
+	 * re-initialies the walpaper after some time
+	 * @returns {void}
+	 */
 	private reInitSystem() {
 		// hide reloader
 		this.reloadHelper.show(false);
@@ -281,42 +333,49 @@ class AudiOrbits extends CComponent {
 	}
 
 	/**
-	* initialize the geometric & grpahics system
-	* => starts rendering loop afterwards
-	* @param {Promise} waitFor wait for this promise if given
-	*/
+	 * initialize the geometric & grpahics system
+	 * => starts rendering loop afterwards
+	 * @param {Promise} waitFor wait for this promise if given
+	 * @returns {void}
+	 */
 	private initSystem(waitFor?: Promise<void>) {
-		Smallog.debug('initializing...');
+		Smallog.debug("initializing...");
 		// show loader
-		this.loadHelper.setText('3D');
+		this.loadHelper.setText("3D");
 		this.loadHelper.setProgress(5);
 		this.loadHelper.show(true);
 
 		// initialize three js and add geometry to returned scene
-		this.ctxHolder.init(waitFor).then(() => {
-			// start auto parallax handler
-			this.swirlInterval = window.setInterval(() => this.swirlHandler(), 1000 / 60);
-			// start rendering
-			this.state = RunState.Running;
-			// hide loader
-			this.loadHelper.show(false);
-			// print
-			Smallog.info('initializing complete.');
-		}).catch((err) => {
-			const m = `Fatal Error when creating main-context!\r\n\r\nMsg: ${err}`;
-			Smallog.error(m);
-			alert(m);
-		});
+		this.ctxHolder
+			.init(waitFor)
+			.then(() => {
+				// start auto parallax handler
+				this.swirlInterval = window.setInterval(
+					() => this.swirlHandler(),
+					1000 / 60
+				);
+				// start rendering
+				this.state = RunState.Running;
+				// hide loader
+				this.loadHelper.show(false);
+				// print
+				Smallog.info("initializing complete.");
+			})
+			.catch((err) => {
+				const m = `Fatal Error when creating main-context!\r\n\r\nMsg: ${err}`;
+				Smallog.error(m);
+				alert(m);
+			});
 	}
-
 
 	// /////////////////////////////////////////////
 	// EVENT HANDLER & TIMERS
 	// /////////////////////////////////////////////
 
 	/**
-	* Auto Parallax handler
-	*/
+	 * Auto Parallax handler
+	 * @returns {void}
+	 */
 	private swirlHandler() {
 		if (this.settings.parallax_option != 2) return;
 		this.swirlStep += this.settings.auto_parallax_speed / 8;
@@ -326,11 +385,9 @@ class AudiOrbits extends CComponent {
 	}
 }
 
-
 // /////////////////////////////////////////////
 // Actual Initialisation
 // /////////////////////////////////////////////
 
 // if the wallpaper is ran in browser, we want to delay the init until caching is complete.
 new WEWWA(() => new AudiOrbits());
-
--- src/LUTHelper.ts
+++ src/LUTHelper.ts
@@ -1,41 +1,48 @@
 /**
-* @author hexxone / https://hexx.one
-*
-* @license
-* Copyright (c) 2021 hexxone All rights reserved.
-* Licensed under the GNU GENERAL PUBLIC LICENSE.
-* See LICENSE file in the project root for full license information.
-*/
+ * @author hexxone / https://hexx.one
+ *
+ * @license
+ * Copyright (c) 2021 hexxone All rights reserved.
+ * Licensed under the GNU GENERAL PUBLIC LICENSE.
+ * See LICENSE file in the project root for full license information.
+ */
 
-import {DataTexture, ImageLoader, Smallog, LinearFilter, NearestFilter, RGBAFormat} from './we_utils/src';
+import {
+	DataTexture,
+	ImageLoader,
+	Smallog,
+	LinearFilter,
+	NearestFilter,
+	RGBAFormat,
+} from "./we_utils/src";
 
 /**
-* LookUpTable Setup helper & holder
-* will locally preload the given textures and set metadata infos
-* @public
-*/
+ * LookUpTable Setup helper & holder
+ * will locally preload the given textures and set metadata infos
+ * @public
+ */
 export class LUTHelper {
 	// set the size to 2 (the identity size). We'll restore it when the
 	// image has loaded. This way the code using the lut doesn't have to
 	// care if the image has loaded or not
 	Textures = [
 		{
-			name: 'posterize',
-			url: './img/lookup/posterize-s8n.png',
+			name: "posterize",
+			url: "./img/lookup/posterize-s8n.png",
 			size: 2,
 			filter: false,
 			texture: null,
 		},
 		{
-			name: 'inverse',
-			url: './img/lookup/inverse-s8.png',
+			name: "inverse",
+			url: "./img/lookup/inverse-s8.png",
 			size: 2,
 			filter: false,
 			texture: null,
 		},
 		{
-			name: 'negative',
-			url: './img/lookup/color-negative-s8.png',
+			name: "negative",
+			url: "./img/lookup/color-negative-s8.png",
 			size: 2,
 			filter: false,
 			texture: null,
@@ -43,8 +50,8 @@ export class LUTHelper {
 	];
 
 	/**
-	* Preload Lookup Table Textures
-	*/
+	 * Preload Lookup Table Textures
+	 */
 	constructor() {
 		this.Textures.forEach((info) => {
 			// if not size set get it from the filename
@@ -59,7 +66,7 @@ export class LUTHelper {
 				const m = /-s(\d+)(n*)\.[^.]+$/.exec(info.url);
 				if (m) {
 					info.size = parseInt(m[1]);
-					info.filter = info.filter === undefined ? m[2] !== 'n' : info.filter;
+					info.filter = info.filter === undefined ? m[2] !== "n" : info.filter;
 				}
 			}
 			info.texture = this.makeLUTTexture(info);
@@ -67,49 +74,63 @@ export class LUTHelper {
 	}
 
 	/**
-	* Create a preloaded texture
-	* @param {Object} info
-	* @return {Texture}
-	*/
+	 * Create a preloaded texture
+	 * @param {Object} info lookUpTableEntryInfo
+	 * @return {Texture} result
+	 */
 	private makeLUTTexture(info) {
 		const imgLoader = new ImageLoader();
-		const ctx = document.createElement('canvas').getContext('2d');
+		const ctx = document.createElement("canvas").getContext("2d");
 
 		let texture = null;
 		if (info.url) {
 			const lutSize = info.size;
-			Smallog.debug('Loading image: ' + JSON.stringify(info));
-			imgLoader.load(info.url, (image) => {
-				const width = lutSize * lutSize;
-				const height = lutSize;
-				info.size = lutSize;
-				ctx.canvas.width = width;
-				ctx.canvas.height = height;
-				ctx.drawImage(image, 0, 0);
-				const imageData = ctx.getImageData(0, 0, width, height);
+			Smallog.debug("Loading image: " + JSON.stringify(info));
+			imgLoader.load(
+				info.url,
+				(image: HTMLImageElement) => {
+					const width = lutSize * lutSize;
+					const height = lutSize;
+					info.size = lutSize;
+					ctx.canvas.width = width;
+					ctx.canvas.height = height;
+					ctx.drawImage(image, 0, 0);
+					const imageData = ctx.getImageData(0, 0, width, height);
 
-				texture = this.makeIdentityLutTexture(imageData.data.buffer, width, height,
-					info.filter ? LinearFilter : NearestFilter);
+					texture = this.makeIdentityLutTexture(
+						imageData.data.buffer,
+						width,
+						height,
+						info.filter ? LinearFilter : NearestFilter
+					);
 
-				texture.needsUpdate = true;
-			}, null, function(err) {
-				Smallog.error('Error loading LUT: ' + err);
-				throw err;
-			});
+					texture.needsUpdate = true;
+				},
+				null,
+				function (err) {
+					Smallog.error("Error loading LUT: " + err);
+					throw err;
+				}
+			);
 		}
 
 		return texture;
 	}
 
 	/**
-		* make filtered texture
-		* @param {BufferSource} data
-		* @param {number} wid
-		* @param {number} hig
-		* @param {TextureFilter} filter
-		* @return {DataTexture}
-		*/
-	private makeIdentityLutTexture(data, wid, hig, filter) {
+	 * make filtered texture
+	 * @param {BufferSource} data to render
+	 * @param {number} wid weidht
+	 * @param {number} hig height
+	 * @param {number} filter Texture filter
+	 * @return {DataTexture} result
+	 */
+	private makeIdentityLutTexture(
+		data: ArrayBufferLike,
+		wid: number,
+		hig: number,
+		filter: number
+	) {
 		const texture = new DataTexture(data, wid, hig, RGBAFormat);
 		texture.minFilter = filter;
 		texture.magFilter = filter;
@@ -118,5 +139,3 @@ export class LUTHelper {
 		return texture;
 	}
 }
-
-
